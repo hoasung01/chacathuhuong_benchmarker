3232b993c39f8733275b76eb2e87f397628a8b10
hoasung01 <nguyenngochai.shipagent@gmail.com>
2025-01-11 12:45:53 +0700

    Add install_generator

---
+++ lib/generators/chacathuhuong_benchmarker/install_generator.rb
@@ -0,0 +1,14 @@
+module ChacathuhuongBenchmarker
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('templates', __dir__)
+
+    def create_initializer
+      template 'initializer.rb', 'config/initializers/chacathuhuong_benchmarker.rb'
+    end
+
+    def create_benchmark_directory
+      empty_directory 'benchmarks'
+      template 'example_benchmark.rb', 'benchmarks/example_benchmark.rb'
+    end
+  end
+end
