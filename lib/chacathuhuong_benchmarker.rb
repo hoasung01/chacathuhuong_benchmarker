4661c59826a739649fd57e91c8d1bba821b9583d
hainguyen-eh <hai.nguyen@employmenthero.com>
2025-01-11 08:23:30 +0700

    first commit

---
+++ lib/chacathuhuong_benchmarker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "chacathuhuong_benchmarker/version"
+
+module ChacathuhuongBenchmarker
+  class Error < StandardError; end
+  # Your code goes here...
+end
