b397ce02b95d14aa24e1a2276fa676dac005c427
hoasung01 <nguyenngochai.shipagent@gmail.com>
2025-01-12 12:49:47 +0700

    Update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ require 'factory_bot'
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  config.formatter = :documentation
+
   config.before(:suite) do
     FactoryBot.find_definitions
 
