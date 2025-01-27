f95f281a64104bd04d927faf2ba1f64412e1f543
hoasung01 <nguyenngochai.shipagent@gmail.com>
2025-01-27 14:08:40 +0700

    Fix: can not rails g install

--- lib/generators/chacathuhuong_benchmarker/install_generator.rb
+++ lib/generators/chacathuhuong_benchmarker/install_generator.rb
@@ -5,12 +5,15 @@ module ChacathuhuongBenchmarker
     source_root File.expand_path('templates', __dir__)
 
     def create_initializer
-      template 'initializer.rb', 'config/initializers/chacathuhuong_benchmarker.rb'
+      template 'config/initializer.rb', 'config/initializers/chacathuhuong_benchmarker.rb'
     end
 
     def create_benchmark_directory
       empty_directory 'benchmarks'
-      template 'example_benchmark.rb', 'benchmarks/example_benchmark.rb'
+      template 'benchmarks/example_benchmark.rb', 'benchmarks/example_benchmark.rb'
+      template 'benchmarks/load_benchmark.rb', 'benchmarks/load_benchmark.rb'
+      template 'benchmarks/memory_benchmark.rb', 'benchmarks/memory_benchmark.rb'
+      template 'benchmarks/query_benchmark.rb', 'benchmarks/query_benchmark.rb'
     end
   end
 end
